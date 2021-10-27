25bdb664ff4b41ab8b156d06a4bfb62820b1c739
AL MAHMUD SURUJ <mahmudsuruj14@gmail.com>
2021-10-27 20:24:29 +0800

    checking

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ run().catch(console.dir);
 app.get('/', (req, res) => {
   res.send('Running the Genius Car Mechanics on Server ')
 });
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
   res.send('Hello updated here  ')
 });
 
